ca014c95993129bf45d2a77fd4779391c6171f1a
jhonatan.bassi <jhonatan.bassi@escola.pr.gov.br>
2026-06-11 09:41:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Espera a página carregar completamente
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Seleciona o botão criado no HTML
+    const botao = document.getElementById("btnAcao");
+
+    // Adiciona um evento de clique ao botão
+    botao.addEventListener("click", function() {
+        alert("Obrigado pelo interesse! O projeto Agrinho incentiva a sustentabilidade e a tecnologia nas escolas.");
+    });
+    
+});
